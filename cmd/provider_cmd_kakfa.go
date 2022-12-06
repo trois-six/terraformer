864d8099c2910201041a02a395b3130fef054ced
Trois-Six <trois.six@free.fr>
2022-12-06 18:02:52 +0100

    feat(WIP): add Kafka provider

--- cmd/provider_cmd_kakfa.go
+++ cmd/provider_cmd_kakfa.go
@@ -15,10 +15,20 @@
 package cmd
 
 import (
+	"os"
+	"strings"
+
 	kafka_terraforming "github.com/GoogleCloudPlatform/terraformer/providers/kafka"
 
 	"github.com/GoogleCloudPlatform/terraformer/terraformutils"
 	"github.com/spf13/cobra"
+)
+
+const (
+	defaultKafkaBootstrapServers = "localhost:9092"
+	defaultKafkaSASLMechanism    = "plain"
+	defaultKafkaSkipTLSVerify    = "false"
+	defaultKafkaTLSEnabled       = "true"
 )
 
 func newCmdKafkaImporter(options ImportOptions) *cobra.Command {
@@ -27,8 +37,46 @@ func newCmdKafkaImporter(options ImportOptions) *cobra.Command {
 		Short: "Import current state to Terraform configuration from Kafka",
 		Long:  "Import current state to Terraform configuration from Kafka",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			bootstrapServers := os.Getenv("KAFKA_BOOTSTRAP_SERVERS")
+			if len(bootstrapServers) == 0 {
+				bootstrapServers = defaultKafkaBootstrapServers
+			}
+
+			caCert := strings.TrimSpace(os.Getenv("KAFKA_CA_CERT"))
+			clientCert := strings.TrimSpace(os.Getenv("KAFKA_CLIENT_CERT"))
+			clientKey := strings.TrimSpace(os.Getenv("KAFKA_CLIENT_KEY"))
+			clientKeyPassphrase := strings.TrimSpace(os.Getenv("KAFKA_CLIENT_KEY_PASSPHRASE"))
+			saslUsername := strings.TrimSpace(os.Getenv("KAFKA_SASL_USERNAME"))
+			saslPassword := strings.TrimSpace(os.Getenv("KAFKA_SASL_PASSWORD"))
+
+			saslMechanism := strings.TrimSpace(os.Getenv("KAFKA_SASL_MECHANISM"))
+			if len(saslMechanism) == 0 {
+				saslMechanism = defaultKafkaSASLMechanism
+			}
+
+			skipTLSVerify := strings.TrimSpace(os.Getenv("KAFKA_SKIP_VERIFY"))
+			if len(skipTLSVerify) == 0 {
+				skipTLSVerify = defaultKafkaSkipTLSVerify
+			}
+
+			tlsEnabled := strings.TrimSpace(os.Getenv("KAFKA_SKIP_VERIFY"))
+			if len(tlsEnabled) == 0 {
+				tlsEnabled = defaultKafkaTLSEnabled
+			}
+
 			provider := newKafkaProvider()
-			err := Import(provider, options, []string{})
+			err := Import(provider, options, []string{
+				bootstrapServers,
+				caCert,
+				clientCert,
+				clientKey,
+				clientKeyPassphrase,
+				saslUsername,
+				saslPassword,
+				saslMechanism,
+				skipTLSVerify,
+				tlsEnabled,
+			})
 			if err != nil {
 				return err
 			}
@@ -44,6 +92,5 @@ func newCmdKafkaImporter(options ImportOptions) *cobra.Command {
 }
 
 func newKafkaProvider() terraformutils.ProviderGenerator {
-
 	return &kafka_terraforming.KafkaProvider{}
 }
